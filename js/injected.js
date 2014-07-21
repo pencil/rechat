f45538748ba3e23499e8975da8db59268ca7b88c
Nils Caspar <ncaspar@me.com>
2014-07-21 14:54:49 +0200

    Remove unused variable

--- js/injected.js
+++ js/injected.js
@@ -1,6 +1,5 @@
 (function() {
-  var player = Twitch && Twitch.player ? Twitch.player.getPlayer() : false,
-      body = $('body');
+  var player = Twitch && Twitch.player ? Twitch.player.getPlayer() : false;
   if (player) {
     setInterval(function() {
       $('body').attr('rechat-video-time', player.getVideoTime());

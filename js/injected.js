843ca1a9292209a951d30280ac78d1f6912d25a1
goldbattle <patfgeneva@gmail.com>
2014-08-02 22:13:39 -0400

    Updated - Check if player function is available

--- js/injected.js
+++ js/injected.js
@@ -2,7 +2,10 @@
   var player = Twitch && Twitch.player ? Twitch.player.getPlayer() : false;
   if (player) {
     setInterval(function() {
-      $('body').attr('rechat-video-time', player.getVideoTime());
+      // Check if function is defined
+      if(typeof(player.getVideoTime) == "function") {
+        $('body').attr('rechat-video-time', player.getVideoTime());
+      }
     }, 200);
   }
 })();

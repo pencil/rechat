b09de2dfbe66b9fabd97353fff42b177638d48e6
Nils Caspar <ncaspar@me.com>
2014-09-20 00:39:00 +0200

    Safari version

--- common/js/injected.js
+++ common/js/injected.js
@@ -2,7 +2,9 @@
   var player = Twitch && Twitch.player ? Twitch.player.getPlayer() : false;
   if (player) {
     setInterval(function() {
-      $('body').attr('rechat-video-time', player.getVideoTime());
+      if(player.getVideoTime) {
+        $('body').attr('rechat-video-time', player.getVideoTime());
+      }
     }, 200);
   }
 })();
--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ for file in common/js/*.js common/vendor/js/*.js
 do
   ln $file ./Chrome/js/
   ln $file ./Firefox/data/
+  ln $file ./Safari.safariextension/js/
 done
 
 for file in common/res/*.*
 do
   ln $file ./Chrome/res/
   ln $file ./Firefox/data/
+  ln $file ./Safari.safariextension/res/
 done

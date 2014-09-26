4a84a673598c6d5cdcb22d52c9378b83cae4de11
Nils Caspar <ncaspar@me.com>
2014-09-26 13:12:53 +0200

    Force link

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 for file in common/js/*.js common/vendor/js/*.js
 do
-  ln $file ./Chrome/js/
-  ln $file ./Firefox/data/
-  ln $file ./Safari.safariextension/js/
+  ln -f $file ./Chrome/js/
+  ln -f $file ./Firefox/data/
+  ln -f $file ./Safari.safariextension/js/
 done
 
 for file in common/res/*.*
 do
-  ln $file ./Chrome/res/
-  ln $file ./Firefox/data/
-  ln $file ./Safari.safariextension/res/
+  ln -f $file ./Chrome/res/
+  ln -f $file ./Firefox/data/
+  ln -f $file ./Safari.safariextension/res/
 done

6a8bebe96fe269b09969d79e6f22cd6a45971941
Nils Caspar <ncaspar@me.com>
2015-02-10 17:02:41 +0100

    Replace UUID with something simpler

--- common/js/rechat.js
+++ common/js/rechat.js
@@ -37,18 +37,10 @@ var ReChat = {
     return null;
   },
 
-  randomUUID: function() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0,
-          v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  },
-
   get: function(path, params, success, failure) {
     switch(ReChat.currentBrowser()) {
       case ReChat.Browser.Safari:
-        var uuid = ReChat.randomUUID(),
+        var uuid = new Date().getTime() + '',
             handler = function(event) {
               if(event.name == uuid) {
                 safari.self.removeEventListener('message', handler);

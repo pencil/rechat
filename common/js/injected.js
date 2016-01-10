083db842bb9c5427f6647df1e7fcc306e1715ab3
Nils Caspar <ncaspar@me.com>
2016-01-10 10:37:12 -0800

    support HTML5 video

--- common/js/injected.js
+++ common/js/injected.js
@@ -1,15 +1,30 @@
 (function(w, d) {
-  var player = $('div#player object');
-  if (player.length) {
+  var playerContainer = $('div#player');
+  if (playerContainer.length) {
     var closer = $('#right_close');
     if (closer.hasClass('closed')) {
       closer.click();
     }
-    setInterval(function() {
-      if(player[0].getVideoTime) {
-        $('body').attr('rechat-video-time', player[0].getVideoTime());
+
+    var player = playerContainer.find('object');
+    if (player.length) {
+      player = player.get(0);
+      setInterval(function() {
+        if(player.getVideoTime) {
+          $('body').attr('rechat-video-time', player.getVideoTime());
+        }
+      }, 200);
+    } else {
+      player = playerContainer.find('video');
+      if (player.length) {
+        player = player.get(0);
+        setInterval(function() {
+          if(player.currentTime) {
+            $('body').attr('rechat-video-time', player.currentTime);
+          }
+        }, 200);
       }
-    }, 200);
+    }
   }
 })(window, document);
 

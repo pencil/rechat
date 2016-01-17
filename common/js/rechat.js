35e20f60c49e5bea227aabb379c9855fb03c7c43
Nils Caspar <ncaspar@me.com>
2016-01-16 23:31:13 -0800

    more SSL

--- common/js/rechat.js
+++ common/js/rechat.js
@@ -1,6 +1,6 @@
 this.ReChat = $.extend({
   // Settings:
-  searchBaseUrl: 'http://search.rechat.org/videos/',
+  searchBaseUrl: 'https://search.rechat.org/videos/',
   chatDisplayLimit: 1000,
   loadingDelay: 3000,
   nicknameColors: Please.make_color({ colors_returned: 50, saturation: 0.7 }),
